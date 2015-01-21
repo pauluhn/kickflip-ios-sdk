3cccaa5bfaf5e7ae7340492557e4369f9eca7ce7
Paul Uhn <paul.uhn@gmail.com>
2015-01-21 15:40:08 -0600

    Create new project

---
+++ kickflip-ios-sdk/kickflip-ios-sdkTests/kickflip_ios_sdkTests.m
@@ -0,0 +1,40 @@
+//
+//  kickflip_ios_sdkTests.m
+//  kickflip-ios-sdkTests
+//
+//  Created by Paul on 1/21/15.
+//  Copyright (c) 2015 Paul. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface kickflip_ios_sdkTests : XCTestCase
+
+@end
+
+@implementation kickflip_ios_sdkTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
